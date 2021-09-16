a278c018972b828b7d2a77ab8b2c6f17a298e89e
KMotiour <kazi_motiour@coredevs.co>
2021-09-17 00:43:14 +0600

    stat

--- musicStore/urls.py
+++ musicStore/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     # urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
